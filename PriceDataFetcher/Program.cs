8de29eaec70c1959f31c4f8e39ac3506f5f8f516
Baurzhan Sabirov <ben.evans@mail.ru>
2025-04-01 16:29:53 +0500

    Added backgroud service to read price every 5 sec

--- PriceDataFetcher/Program.cs
+++ PriceDataFetcher/Program.cs
@@ -1,5 +1,7 @@
 using Asp.Versioning;
 using PriceDataFetcher.Service;
+using PriceDataFetcher.Service.Background;
+using Serilog;
 
 namespace PriceDataFetcher
 {
@@ -7,63 +9,85 @@ namespace PriceDataFetcher
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            //CORS settings
-            var corsOrigins = "SynteticArbitrage";
-            builder.Services.AddCors(options =>
+            Log.Logger = new LoggerConfiguration()
+               .ReadFrom.Configuration(new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                   .Build())
+               .CreateLogger();
+            try
             {
-                options.AddPolicy(name: corsOrigins,
-                                  policy =>
-                                  {
-                                      policy
-                                      .AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-                                  });
-            });
+                Log.Information("Starting up the SynteticArbitrage PriceDataFetcher API...");
+                var builder = WebApplication.CreateBuilder(args);
 
-            //API Versioning
-            builder.Services.AddApiVersioning(options => {
-                options.DefaultApiVersion = new ApiVersion(1, 0);
-                options.AssumeDefaultVersionWhenUnspecified = true;
-                options.ReportApiVersions = true;
-                options.ApiVersionReader = ApiVersionReader.Combine(
-                    new UrlSegmentApiVersionReader(),
-                    new HeaderApiVersionReader("X-Api-Version"),
-                    new MediaTypeApiVersionReader("v")
-                );
-            })
-           .AddApiExplorer(options => {
-               options.GroupNameFormat = "'v'VVV";
-               options.SubstituteApiVersionInUrl = true;
-           });
-            // Add services to the container.
-            builder.Services.AddScoped<ISymbolInfoReader, BinanceSymbolInfoReader>();
-            builder.Services.AddScoped<IBTCSymbolReader, BTCSymbolReader>();
-            builder.Services.AddScoped<IApiReader, ApiReader>();
-            builder.Services.AddScoped<IPriceReader, BinancePriceReader>();
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+                //CORS settings
+                var corsOrigins = "SynteticArbitrage";
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy(name: corsOrigins,
+                                      policy =>
+                                      {
+                                          policy
+                                          .AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                                      });
+                });
 
-            var app = builder.Build();
+                //API Versioning
+                builder.Services.AddApiVersioning(options => {
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.ReportApiVersions = true;
+                    options.ApiVersionReader = ApiVersionReader.Combine(
+                        new UrlSegmentApiVersionReader(),
+                        new HeaderApiVersionReader("X-Api-Version"),
+                        new MediaTypeApiVersionReader("v")
+                    );
+                })
+               .AddApiExplorer(options => {
+                   options.GroupNameFormat = "'v'VVV";
+                   options.SubstituteApiVersionInUrl = true;
+               });
+                // Add services to the container.
+                builder.Services.AddSingleton<ISymbolInfoReader, BinanceSymbolInfoReader>();
+                builder.Services.AddSingleton<IBTCSymbolReader, BTCSymbolReader>();
+                builder.Services.AddSingleton<IApiReader, ApiReader>();
+                builder.Services.AddSingleton<IPriceReader, BinancePriceReader>();
+                builder.Services.AddHostedService<BackgroundPriceReader>();
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {                
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                builder.Host.UseSerilog();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.UseCors(corsOrigins);
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.UseCors(corsOrigins);
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
     }
 }
---
+++ PriceDataFetcher/Service/Background/BackgroundPriceReader.cs
@@ -0,0 +1,37 @@
+﻿
+namespace PriceDataFetcher.Service.Background
+{
+    public class BackgroundPriceReader : BackgroundService
+    {
+        private readonly IPriceReader _priceReader;
+        private readonly IBTCSymbolReader _symbolReader;
+
+        public BackgroundPriceReader(IPriceReader priceReader, IBTCSymbolReader symbolReader)
+        {
+            _priceReader = priceReader;
+            _symbolReader = symbolReader;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var symbol = await _symbolReader.GetSymbolAsync("CURRENT_QUARTER");
+            PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+            while(await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                // Fetch price data
+                Console.WriteLine("Reading price...");
+                var result = await _priceReader.GetKlinesAsync(symbol, "1d");
+                if (result.IsSuccess)
+                {
+                    Console.WriteLine("Price read successfully");
+                    var price = result.Value?.Last();
+                    Console.WriteLine(price);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to read price. " + result.ErrorMessage);
+                }
+            }
+        }
+    }
+}
